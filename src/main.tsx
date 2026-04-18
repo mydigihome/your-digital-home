d97c41cb90c2fad2fc8c9a8cb1e82bbb4d9bee24
mydigihome <myslimher@gmail.com>
2026-04-17 23:00:04 -0600

    Call bootstrapTheme() before React mounts to fix dark/light mode flash and stale dark mode state

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { bootstrapTheme } from "./lib/localStorage";
+
+// Apply theme BEFORE React renders to prevent flash of wrong color scheme
+bootstrapTheme();
 
 createRoot(document.getElementById("root")!).render(<App />);
